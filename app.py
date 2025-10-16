7ea2f35a9399aab64c1b2663f255aa1d4d6d5302
Shivam Kumar <shivamkcool232@gmail.com>
2025-10-16 23:49:01 +0530

    Add homepage route and home.html template

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ with app.app_context():
     db.create_all()
 
 # --- Routes ---
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 @app.route('/')
 def index():
     tasks = Task.query.all()
